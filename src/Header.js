11464072af1d8b2bb85faa78d54dcd991a2aeb36
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-09-28 13:40:23 +0530

    tinder clone client changes

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Header.css";
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize='large' className='header__icon' />
+            </IconButton>
+            <img
+                className='header__logo'
+                src='https://uxwing.com/wp-content/themes/uxwing/download/10-brands-and-social-media/tinder.png'
+                alt='tinder image'
+            />
+            <IconButton>
+                <ForumIcon fontSize='large' className='header__icon' />
+            </IconButton>
+
+        </div>
+    );
+}
+
+export default Header;
